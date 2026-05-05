cffb091f8a71cd297ca86cd674b923a6704d3ca5
Lilysheng <138137425+Annalenashengbb@users.noreply.github.com>
2026-05-05 23:33:55 +0100

    Update config.js

--- cards/card-093/config.js
+++ cards/card-093/config.js
@@ -23,18 +23,18 @@ window.CARD_CONFIG = {
   debugHands: false,
 
   // 这张卡的模型组合
-  // 左手 green，右手 blue
+  // 左手 grey，右手 brown
   attachments: [
     {
       parent: "leftHand",
-      src: "/assets/models/green.glb",
+      src: "/assets/models/grey.glb",
       position: "0 0.003 0",
       rotation: "0 0 0",
       scale: "1 1 1"
     },
     {
       parent: "rightHand",
-      src: "/assets/models/blue.glb",
+      src: "/assets/models/brown.glb",
       position: "0 0.003 0",
       rotation: "0 0 0",
       scale: "1 1 1"
