9e1e553b725ea63845a72815cf157377a3517af4
Lilysheng <138137425+Annalenashengbb@users.noreply.github.com>
2026-05-05 02:46:57 +0100

    Update config.js

--- cards/card-001/config.js
+++ cards/card-001/config.js
@@ -4,7 +4,7 @@ window.CARD_CONFIG = {
 
   // 整个“小猫+手上物件”组合在卡面上的位置、方向、大小
   catRoot: {
-    position: "0 -0.012 0.001",
+    position: "0 -0.02 0.001",
     rotation: "0 0 0",
     scale: "36 36 36"
   },
